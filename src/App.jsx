4beabb976d06d726000faefcbb23063cc4ff8603
GodFilmmy <135239279+GodFilmmy@users.noreply.github.com>
2025-02-06 22:14:19 +0700

    ScheduleFirstCreate

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ReservationForm from "./ReservationForm/ReservationForm";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import TopNavBar from "./NavBar/Nav";
-
+import ScheduleResult from "./SchedulePage/Schedulepage";
 function App() {
   return (
     <>
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/form" element={<ReservationForm />} />
+          <Route path="/schdule" element={<ScheduleResult />} />
         </Routes>
       </BrowserRouter>
       <Footer />
