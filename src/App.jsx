6c32527a0a397e71a906dd47588fb2b107310a54
GodFilmmy <135239279+GodFilmmy@users.noreply.github.com>
2025-02-07 18:28:15 +0700

    MergeUpdate

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,23 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import TopNavBar from "./NavBar/Nav";
 import ScheduleResult from "./SchedulePage/Schedulepage";
-import Footer from './Footer/Footer';
-import { ReversationProvider } from './contexts/useReversation'
+import { ReversationProvider } from "./contexts/useReversation";
 function App() {
-
   return (
     <>
       <BrowserRouter>
-      <TopNavBar/>
+        <TopNavBar />
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/form" element={<ReservationForm />} />
+          <Route path="/" element={<HomePage/>} />
           <Route path="/schdule" element={<ScheduleResult />} />
-          <Route path="/form" element={
-            <ReversationProvider>
-              <ReservationForm />
-            </ReversationProvider>
-          } />
+          <Route
+            path="/form"
+            element={
+              <ReversationProvider>
+                <ReservationForm />
+              </ReversationProvider>
+            }
+          />
         </Routes>
       </BrowserRouter>
       <Footer></Footer>
