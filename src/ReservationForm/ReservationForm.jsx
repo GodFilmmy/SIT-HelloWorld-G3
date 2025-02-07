ee3f1500706659c1fea3d899f97ac094a2cf6a4d
GodFilmmy <135239279+GodFilmmy@users.noreply.github.com>
2025-02-07 18:49:06 +0700

    A litt;e change in reservation

--- src/ReservationForm/ReservationForm.jsx
+++ src/ReservationForm/ReservationForm.jsx
@@ -40,12 +40,12 @@ const ReservationForm = () => {
 
   return (
     <>
-      <div className="flex flex-col pt-20 justify-center bg-[url(Form-img/form-background.png)] items-center bg-center bg-cover bg-no-repeat p-5">
+      <div className="form-page-container flex flex-col pt-20 justify-center bg-[url(Form-img/form-background.png)] items-center bg-center bg-cover bg-no-repeat p-5">
         {/* <div className="max-w-[1200px] w-full h-full mx-auto rounded-lg bg-white px-16 py-15 bg-center "> */}
         {/* <div className='bg-white p-6 rounded-lg shadow-lg w-[1127px] max-w-4xl'> */}
-        <div className="bg-white bg-opacity-90 backdrop-blur-md p-6 rounded-lg shadow-lg w-full max-w-4xl mx-auto mt-10 ">
+        <div className="form-box-container bg-white bg-opacity-90 backdrop-blur-md p-6 rounded-lg shadow-lg w-full max-w-4xl mx-auto mt-10 ">
           <div className="flex flex-col gap-2">
-            <h1 className="text-2xl py-3 px-5 bg-gray-900 text-white text-center font-bold uppercase">
+            <h1 className="from-header text-2xl py-3 px-5 bg-gray-900 text-white text-center font-bold uppercase">
               Room Booking
             </h1>
           </div>
@@ -60,7 +60,7 @@ const ReservationForm = () => {
                 </div> */}
 
           {/* <div className='flex ml-auto gap-10 justify-between'> */}
-          <div className="grid grid-cols-2 mt-4 items-center justify-center pb-1">
+          <div className="form-time-detail grid grid-cols-2 mt-4 items-center justify-center pb-1">
             <div className="flex items-center justify-center">
               <CalendarFrom />
             </div>
